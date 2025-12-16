4d051ebfdc9736a9f70f4076f822f351ffed4f6d
Aleksey Seledcov <alex-ozu@yandex.ru>
2025-12-16 06:38:00 +0300

    Дипломный проект. Часть 2 попытка 3

--- main/src/main/java/ru/practicum/user/dto/UserDto.java
+++ main/src/main/java/ru/practicum/user/dto/UserDto.java
@@ -2,10 +2,14 @@ package ru.practicum.user.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 // Пользователь
+@NoArgsConstructor
+@AllArgsConstructor
 @Getter
 @Setter
 public class UserDto {
